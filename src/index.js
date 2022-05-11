e131d9a34795c9c3cde997f971de161560f60cdd
mikhail.soosaar <mikhail.soosaar@bigwintek.com>
2022-05-11 11:30:48 +0300

    Simple implementation of React just for fun.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Armory} from "./Armory";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/** @jsx Armory.createElement */
+function Counter() {
+    const [state, setState] = Armory.useState(1)
+    return (
+        <div>
+            <h1>
+                Count: {state}
+            </h1>
+            <button
+                onClick={() => setState(c => c + 1)}
+            >
+                Click me
+            </button>
+        </div>
+    )
+}
+const element = <Counter />
+const container = document.getElementById("root")
+Armory.render(element, container)
